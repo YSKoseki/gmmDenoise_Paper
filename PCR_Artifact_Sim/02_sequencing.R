955bbf63fb064f7e96133229fefba4a19c6b5d72
YSKoseki <ykoseki@otsuma.ac.jp>
2022-10-08 14:14:38 +0900

    Calculate range for no. of sequences and reads

--- PCR_Artifact_Sim/02_sequencing.R
+++ PCR_Artifact_Sim/02_sequencing.R
@@ -78,7 +78,7 @@ summary_tib <- sequencing %>% nest_by(repl, seq_type) %>%
   ) %>%
   select(repl, seq_type, seq_n, sum_reads)
 write_csv(summary_tib, paste0(path_out, "/02-summary_tib.csv"))
-summary_stats_tib <- summary_tib %>% nest_by(seq_type) %>%
+meansd_tib <- summary_tib %>% nest_by(seq_type) %>%
   mutate(
     mean_seq_n = mean(data$seq_n),
     sd_seq_n = sd(data$seq_n),
@@ -86,7 +86,21 @@ summary_stats_tib <- summary_tib %>% nest_by(seq_type) %>%
     sd_sum_reads = sd(data$sum_reads)
   ) %>%
   select(seq_type, mean_seq_n, sd_seq_n, mean_sum_reads, sd_sum_reads)
-write_csv(summary_stats_tib, paste0(path_out, "/03-summary_stats_tib.csv"))
+write_csv(meansd_tib, paste0(path_out, "/03-meansd_tib.csv"))
+minmax_read_tib <- sequencing %>% nest_by(seq_type) %>%
+  mutate(
+    min_read_n = min(data$reads),
+    max_read_n = max(data$reads)
+  ) %>%
+  select(seq_type, min_read_n, max_read_n)
+minmax_seq_tib <- summary_tib %>% nest_by(seq_type) %>%
+  mutate(
+    min_seq_n = min(data$seq_n),
+    max_seq_n = max(data$seq_n)
+  ) %>%
+  select(seq_type, min_seq_n, max_seq_n)
+minmax_tib <- full_join(minmax_seq_tib, minmax_read_tib, by = "seq_type")
+write_csv(minmax_tib, paste0(path_out, "/04-minmax_tib.csv"))
 
 # Save R ojbects
 saveRDS(sequencing, paste0(path_obj, "/sequencing.obj"))
